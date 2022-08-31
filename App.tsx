23d07a7b20dab8124ae140589de556ae11d7f15b
rs-v <w42023@outlook.com>
2022-08-31 20:18:02 +0800

    1 file modified

--- App.tsx
+++ App.tsx
@@ -6,6 +6,15 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <h2>Start editing to see some magic happen :)</h2>
+      <button >hello </button>
+      <script type="text/javascript">
+        let a;
+              var aco=function(){ 
+              
+              
+              }
+
+      </script>
     </div>
   );
 }
